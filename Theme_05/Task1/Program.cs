ac0678e6938aabd70e23dc0454169c07d660a1f6
Maxim Vitugov <mvitugov@gmail.com>
2023-12-26 01:58:23 +0100

    regular

---
+++ Theme_05/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Program
+{    
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Input sentence, please:");
+            string inputText = Console.ReadLine();
+            string[] words = SplitText(inputText);
+            Console.WriteLine("Every word in this sentence:");
+            PrintWords(words);
+        }
+
+        public static string[] SplitText(string text)
+        {
+            string[] words = text.Split(' ');
+            return words;
+        }
+
+        public static void PrintWords(string[] words)
+        {
+            foreach (string word in words)
+                Console.WriteLine(word);
+        }
+    }
+}
---
+++ Theme_05/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Input sentence, please:");
+            string inputPhrase = Console.ReadLine();
+            string reversedPhrase = ReverseWords(inputPhrase);
+            Console.WriteLine("Reversed sentence:");
+            Console.WriteLine(reversedPhrase);
+        }
+
+        static string ReverseWords(string inputPhrase)
+        {
+            var words = SplitText(inputPhrase).ToArray();
+            var result = string.Join(" ", words.Reverse()).Trim();
+            return result;
+        }
+
+        static string[] SplitText(string text) => text.Split(' ');
+    }
+}
